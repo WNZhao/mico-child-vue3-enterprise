a1b6ba664b25b3284322e50bbf7dace1d94386e5
Walker <zw37520@gmail.com>
2025-04-18 13:26:23 +0700

    chore: 更新路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,16 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-04-17 14:52:44
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-04-18 11:19:59
+ * @LastEditTime: 2025-04-18 13:09:15
  * @FilePath: /micro-child-vue3-enterprise/src/router/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import FindEnterprise from '../views/findEnterprise.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
